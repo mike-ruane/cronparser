c5dfc629827ea907a98484211b264ba38cf2c201
mike-ruane <mike.jruane@gmail.com>
2017-11-21 20:58:19 +0000

    fix args issues from sbt run to execute jar

--- src/main/scala/cronparser/CronParser.scala
+++ src/main/scala/cronparser/CronParser.scala
@@ -4,8 +4,9 @@ import util.OutputFormatter._
 
 object CronParser extends App {
 
-  val command = args.reverse.head
-  val cronValueArgs = args.reverse.tail.reverse.zipWithIndex
+  val argsList = args.flatMap(_.split(" "))
+  val command = argsList.reverse.head
+  val cronValueArgs = argsList.reverse.tail.reverse.zipWithIndex
 
   val cronEvaluator = cronValueArgs.flatMap { case (v, i) =>
     val maybeCronMeta = indexMap.get(i)
@@ -19,7 +20,7 @@ object CronParser extends App {
   if (validCron) {
     Header
     cronEvaluator.foreach { res => successCronFormat(res)}
-    System.out.format(s"%16s%31s", "command" + "|", s"$command")
+    System.out.format(s"%16s%100s", "command" + "|", s"$command")
     println("\n")
   } else {
     ErrorHeader
